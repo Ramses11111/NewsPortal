92ac77cf4e28d9f7867571e7d8981ef60599380d
Roman_Novikov <ramses11111@mail.ru>
2022-12-28 19:55:59 +0300

    Static

---
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+bad_words = [
+    'редиска',
+    'сосиска',
+    'навухудоносор',
+    'петух гамбурский',
+    'моргалы выколю',
+]
+
+@register.filter(name="censor")
+def censor(value):
+    if not isinstance(value, str):
+        raise TypeError(f"Ошибка: переменная {value} должна быть строкой")
+    for bw in value.split():
+        if bw.lower() in bad_words:
+            value = value.replace(bw, list(bw)[0] + ((len(bw) - 1) * "*"))
+    return value
