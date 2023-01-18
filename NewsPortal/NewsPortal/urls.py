30a41b35260dbd41af769ef8a10aed962c9b58c2
Roman_Novikov <ramses11111@mail.ru>
2023-01-19 02:10:19 +0300

    Исправил баг с регистрацией

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('accounts/', include('allauth.urls')),
-    path('accounts/login/', include('allauth.urls')),
-    path('accounts/signup/', include('allauth.urls')),
-    path('accounts/logout/', include('allauth.urls')),
+    path('accounts/login/', include('accounts.urls')),
+    path('accounts/signup/', include('accounts.urls')),
+    path('accounts/logout/', include('accounts.urls')),
     path('news/', include('news.urls')),
 ]
